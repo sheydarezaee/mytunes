f8d6dd516c7fd33c4d0ad5f7a0aecc43d8431cd4
Hack Reactor Student <student@hackreactor.com>
2017-04-10 21:32:55 -0700

    Completed SongModel and SongQueue Collection tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,8 +2,20 @@
 var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
-
+  
   initialize: function() {
-  }
+    this.on('add remove', function(e) {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
 
+    this.on('ended dequeue', function(e) {
+      this.remove(this.models[0]);
+    });
+  },
+
+  playFirst: function () {
+    this.models[0].play();
+  }
 });
